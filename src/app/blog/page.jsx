1190a4afa7e0b40877c094bfddcdcb4288e6226e
Im-Humor <orangite@gmail.com>
2024-01-23 13:17:47 -0700

    add admin authentication and redirection when logged in and navigating to blog

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import "@/app/globalstyle.css";
 import "@/app/aboutbarstyle.css";
 import "@/app/blog/blogstyle.css";
@@ -7,23 +5,14 @@ import Header from "@/app/Header";
 import Footer from "@/app/footer";
 import BlogHero from "@/app/blog/bloghero";
 import PostSection from "@/app/blog/postsection";
-import { useState, useEffect } from "react";
-import { getCookie, getCookies } from "cookies-next";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default function Blog() {
-	const [loggedIn, setLoggedIn] = useState(false);
-
-	useEffect(() => {
-		console.log(getCookies());
-		const access_cookie = getCookie("access_token");
-		console.log(access_cookie);
-		if (access_cookie) {
-			setLoggedIn(true);
-		}
-	}, []);
-
-	if (loggedIn) {
-		window.location.href = "/blog/admin";
+	const cookieStore = cookies();
+	const access_token = cookieStore.get("access_token");
+	if (access_token) {
+		redirect("/blog/admin");
 	} else {
 		return (
 			<div className="blog-container">
@@ -32,7 +21,6 @@ export default function Blog() {
 				<div className="white-div under-hero">
 					<PostSection />
 				</div>
-
 				<div className="footer-div-black">
 					<Footer />
 				</div>
